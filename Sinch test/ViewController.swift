0acb6c7dd2ffa3204f3d45ad921115094d71fc0e
Logesh R <logesh45@gmail.com>
2017-11-17 07:33:33 +0530

    Initial Commit

---
+++ Sinch test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sinch test
+//
+//  Created by Logesh R on 17/11/17.
+//  Copyright © 2017 Logesh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sinch testTests/Sinch_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sinch_testTests.swift
+//  Sinch testTests
+//
+//  Created by Logesh R on 17/11/17.
+//  Copyright © 2017 Logesh R. All rights reserved.
+//
+
+import XCTest
+@testable import Sinch_test
+
+class Sinch_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
